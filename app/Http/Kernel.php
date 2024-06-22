8460ef8bf2d37058baa538208295c79b8f7bf8da
Brayan Carrillo <brayancarrillo429@gmail.com>
2024-06-22 14:12:33 -0500

    a

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,3 +2,15 @@ protected $middleware = [
     // Otros middleware...
     \App\Http\Middleware\CorsMiddleware::class,
 ];
+
+protected $middlewareGroups = [
+    'web' => [
+        // Otros middlewares...
+        \App\Http\Middleware\VerifyCsrfToken::class,
+    ],
+
+    'api' => [
+        // Otros middlewares...
+        \App\Http\Middleware\VerifyCsrfToken::class,
+    ],
+];
