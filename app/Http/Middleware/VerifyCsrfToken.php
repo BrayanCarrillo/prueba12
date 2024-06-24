33d1019c05f72f0e4c50ec267d3b62817f194100
Brayan Carrillo <brayancarrillo429@gmail.com>
2024-06-24 15:32:56 -0500

    ojala funcione

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -1,18 +1,17 @@
 <?php
+
 namespace App\Http\Middleware;
 
-use Closure;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
-class Cors
+class VerifyCsrfToken extends Middleware
 {
-    public function handle($request, Closure $next)
-    {
-        $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Origin', '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        return $response;
-    }
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array<int, string>
+     */
+    protected $except = [
+        //
+    ];
 }
